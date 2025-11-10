bd8a0019fa1509e1e851fc3f9a64adee56d91fa2
badr alkowni <badr.alkowni@thetradedesk.com>
2025-11-10 00:30:48 -0800

    bug fixes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-document.querySelector('form').addEventListener('submit', e => {
-    e.preventDefault();
-    alert('Thanks for reaching out! We’ll get back to you soon.');
-});
+<!-- LIGHTBOX HTML (new addition) -->
+<div class="lightbox" id="lightbox">
+    <div class="lightbox-content">
+        <button class="lightbox-close" id="lightboxClose">×</button>
+        <img src="" alt="" id="lightboxImg">
+            <button class="lightbox-nav prev" id="lightboxPrev">‹</button>
+            <button class="lightbox-nav next" id="lightboxNext">›</button>
+    </div>
+</div>
+
+<!-- JS should be loaded at bottom -->
+<script src="js/main.js"></script>
